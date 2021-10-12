ec0d16f80fab70d03d97a8d436397694e9ee1ace
Lisa Xu <lisaxu220@hotmail.com>
2021-10-12 18:22:01 +0400

    Start making hero banner

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import "./Intro.scss";
 
 function Intro() {
   return (
     <div className="intro" id="intro">
-      Hello
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/headshot.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi, I'm</h2>
+          <h1>Lisa</h1>
+          <h3>Fullstack <span>Developer</span></h3>
+        </div>
+      </div>
     </div>
   )
 }
